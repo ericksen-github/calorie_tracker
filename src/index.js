fa9e030fc09fe148c1de426e401aab17308113e8
James <ericksen.github@gmail.com>
2020-06-05 10:44:36 +0100

    Attempting to resolve chart loading issue.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { lineChart } from "./modules/Chart.js";
 import { createInputForm } from "./modules/createInputForm";
 
 lineChart;
+console.log("index on");
 
 document.getElementById("createFormButton").addEventListener("click", () => {
   createInputForm();
