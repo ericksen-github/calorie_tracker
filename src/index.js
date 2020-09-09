f641459a278a8f019184de35a1b73df13dd7ec68
James <ericksen.github@gmail.com>
2020-09-09 14:59:22 +0100

    Condensed inital addListeners to new module.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
-import { createInputForm } from "./modules/createInputForm";
-import { tableFunctions } from "./modules/tableFunctions";
-import { graphSelector } from "./modules/graphSelector";
-import { localStorageFunctions, allData } from "./modules/localStorage";
-import { showDemoArray } from "./modules/demoData";
-import { clearData } from "./modules/clearData";
+import { localStorageFunctions } from "./modules/localStorage";
+import { createInitialListeners } from "./modules/createInitialListeners";
 
 if (localStorageFunctions.storageAvailable("localStorage")) {
   localStorageFunctions.populateStorage();
 }
 
-document.getElementById("createFormButton").addEventListener("click", () => {
-  createInputForm();
-});
-
-// Adds listeners to swap button with active class to the selected button (highlight it)
-// also adds listener for swapping graph display based on selected data type
-const btns = document.getElementsByClassName("btns");
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    const current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-    graphSelector.selectGraph();
-  });
-}
-
-const demoButton = document.getElementById("demoButton");
-demoButton.addEventListener("click", () => {
-  showDemoArray();
-});
-
-const clearButton = document.getElementById("clearButton");
-clearButton.addEventListener("click", () => {
-  clearData();
-});
-
-// Adds listener to Date header for sorting options on click
-document.getElementById("dateHeader").addEventListener("click", () => {
-  tableFunctions.swapTrackerDirection();
-});
+createInitialListeners();
---
+++ src/modules/createInitialListeners.js
@@ -0,0 +1,40 @@
+import { createInputForm } from "./createInputForm";
+import { tableFunctions } from "./tableFunctions";
+import { graphSelector } from "./graphSelector";
+import { showDemoArray } from "./demoData";
+import { clearData } from "./clearData";
+
+const createInitialListeners = () => {
+  document.getElementById("createFormButton").addEventListener("click", () => {
+    createInputForm();
+  });
+
+  // Adds listeners to swap button with active class to the selected button (highlight it)
+  // also adds listener for swapping graph display based on selected data type
+  const btns = document.getElementsByClassName("btns");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      const current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+      graphSelector.selectGraph();
+    });
+  }
+
+  const demoButton = document.getElementById("demoButton");
+  demoButton.addEventListener("click", () => {
+    showDemoArray();
+  });
+
+  const clearButton = document.getElementById("clearButton");
+  clearButton.addEventListener("click", () => {
+    clearData();
+  });
+
+  // Adds listener to Date header for sorting options on click
+  document.getElementById("dateHeader").addEventListener("click", () => {
+    tableFunctions.swapTrackerDirection();
+  });
+};
+
+export { createInitialListeners };
